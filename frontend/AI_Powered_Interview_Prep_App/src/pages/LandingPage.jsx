cf1fd338654993d012f43b6d000a93709d73ad7c
bled1908 <bled19082005@gmail.com>
2025-09-02 22:59:03 +0530

    Initial setup

---
+++ frontend/AI_Powered_Interview_Prep_App/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// update hero image that is dashboard image and save it in the given address below
+import HERO_IMG from "../assets/hero-img.png";
+import { APP_FEATURES } from "../utils/data";
+
+const LandingPage = () => {
+    return (
+        <div>LandingPage</div>
+    )
+}
+
+export default LandingPage
