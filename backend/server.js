7c4e24fdb916cdc1c87ac002f1af78e40ec59e83
bled1908 <bled19082005@gmail.com>
2025-09-10 01:23:48 +0530

    auth route done

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 // Middleware to handle CORS
@@ -21,6 +23,12 @@ connectDB();
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
+// app.use('/api/sessions', sessionRoutes);
+// app.use('/api/questions', questionRoutes);
+
+// app.use('/api/ai/generate-questions', protect, generateInterviewQuestions);
+// app.use('/api/ai/generate-explanation', protect, generateConceptExplanation);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
