ed1c7cc5cf132ced55ec2ee00a70aa561e3cb6a9
bled1908 <bled19082005@gmail.com>
2025-09-12 10:30:29 +0530

    Integrating Sign-Up-API

--- frontend/AI_Powered_Interview_Prep_App/src/pages/Auth/SignUp.jsx
+++ frontend/AI_Powered_Interview_Prep_App/src/pages/Auth/SignUp.jsx
@@ -3,6 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
 import { validateEmail } from '../../utils/helper';
+import { UserContext } from '../../context/userContext';
+import { useContext } from 'react';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import uploadImage from '../../utils/uploadImage';
 
 const SignUp = ({setCurrentPage}) => {
     const [profilePic, setProfilePic] = useState(null);
@@ -12,6 +17,7 @@ const SignUp = ({setCurrentPage}) => {
 
     const [error, setError] = useState(null);
 
+    const { updateUser } = useContext(UserContext);
     const navigate = useNavigate();
 
     // Handle Sign Up Form Submit
@@ -39,7 +45,26 @@ const SignUp = ({setCurrentPage}) => {
 
         // Sign Up API Call
         try {
-            
+            // Upload image if present
+            if (profilePic) {
+                const imgUploadRes = await uploadImage(profilePic);
+                profileImageUrl = imgUploadRes.imageUrl || "";
+            }
+
+            const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+                name: fullName,
+                email,
+                password,
+                profileImageUrl,
+            });
+
+            const { token } = response.data;
+
+            if (token) {
+                localStorage.setItem("token", token);
+                updateUser(response.data)
+                navigate("/dashboard");
+            }
         } catch (error) {
             if(error.response && error.response.data.message) {
                 setError(error.response.data.message);
