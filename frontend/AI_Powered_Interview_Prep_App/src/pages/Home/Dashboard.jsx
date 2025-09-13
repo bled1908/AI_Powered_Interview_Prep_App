94f6ddbb0d7becf1fe1c3ab418851bdaec92f8dc
bled1908 <bled19082005@gmail.com>
2025-09-13 22:50:04 +0530

    fetching all sessions from backend, creating dashboard summary card ui

--- frontend/AI_Powered_Interview_Prep_App/src/pages/Home/Dashboard.jsx
+++ frontend/AI_Powered_Interview_Prep_App/src/pages/Home/Dashboard.jsx
@@ -3,7 +3,11 @@ import { LuPlus } from 'react-icons/lu';
 import { CARD_BG } from '../../utils/data';
 import toast from 'react-hot-toast';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
-import { data, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import SummaryCard from '../../components/Cards/SummaryCard';
+import moment from 'moment';
 
 const Dashboard = () => {
     const navigate = useNavigate();
@@ -17,6 +21,12 @@ const Dashboard = () => {
     });
 
     const fetchAllSessions = async () => {
+        try {
+            const response = await axiosInstance.get(API_PATHS.SESSION.GET_ALL);
+            setSessions(response.data);
+        } catch (error) {
+            console.error("Error fetching session data:", error);
+        }
     };
 
     const deleteSession = async (sessionData) => {};
@@ -28,7 +38,24 @@ const Dashboard = () => {
         <DashboardLayout>
             <div className="container mx-auto pt-4 pb-4">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-7 pt-1 pb-6 px-4 md:px-0">
-
+                    {sessions?.map((data, index) => (
+                        <SummaryCard
+                            key={data?._id}
+                            colors={CARD_BG[index % CARD_BG.length]}
+                            role={data?.role || ""}
+                            topicsToFocus={data?.topicsToFocus || ""}
+                            experience={data?.experience || "-"}
+                            questions={data?.questions?.length || "-"}
+                            description={data?.description || ""}
+                            lastUpdated={
+                                data?.updatedAt
+                                    ? moment(data.updatedAt).format("Do MMM YYYY")
+                                    : ""
+                            }
+                            onSelect={() => navigate(`/interview-prep/${data?._id}`)}
+                            onDelete={() => setOpenDeleteAlert({ open: true, data })}
+                        />
+                    ))}
                 </div>
 
                 <button
