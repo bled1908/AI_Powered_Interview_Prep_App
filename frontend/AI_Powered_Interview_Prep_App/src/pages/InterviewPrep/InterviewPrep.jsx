1ba9f527a0bd896a50c9158039e4ac75cb59a897
bled1908 <bled19082005@gmail.com>
2025-09-14 11:29:06 +0530

    api call to create new session, building interview prep page ui

--- frontend/AI_Powered_Interview_Prep_App/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/AI_Powered_Interview_Prep_App/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import moment from 'moment';
+import { AnimatePresence, motion } from 'framer-motion';
+import { LuCircleAlert, LuListCollapse } from 'react-icons/lu';
+import SpinnerLoader from '../../components/Loader/SpinnerLoader';
+import { toast } from 'react-hot-toast';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import RoleInfoHeader from './components/RoleInfoHeader';
 
 const InterviewPrep = () => {
-    return (
-        <div>InterviewPrep</div>
-    )
-}
+    const { sessionId } = useParams();
 
-export default InterviewPrep
+    const [sessionData, setSessionData] = useState(null);
+    const [errorMsg, setErrorMsg] = useState("");
+
+    const [openLeanMoreDrawer, setOpenLeanMoreDrawer] = useState(false);
+    const [explanation, setExplanation] = useState(null);
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [isUpdateLoader, setIsUpdateLoader] = useState(false);
+
+    // Fetch session data by session id
+    const fetchSessionDetailsById = async () => {
+        
+    };
+
+    // Generate Concept Explanation
+    const generateConceptExplanation = async (question) => {};
+
+    // Pin Question
+    const toggleQuestionPinStatus = async (questionId) => {};
+
+    // Add more questions to a session
+    const uploadMoreQuestions = async () => {};
+
+    useEffect(() => {
+        if (sessionId) {
+            fetchSessionDetailsById();
+        }
+
+        return () => {};
+    }, []);
+    return (
+        <DashboardLayout>
+            <RoleInfoHeader
+                role={sessionData?.role || ""}
+                topicsToFocus={sessionData?.topicsToFocus || ""}
+                experience={sessionData?.experience || "-"}
+                questions={sessionData?.questions?.length || "-"}
+                description={sessionData?.description || ""}
+                lastUpdated={
+                    sessionData?.updatedAt
+                        ? moment(sessionData.updatedAt).format("Do MMM YYYY")
+                        : ""
+                }
+            />
+        </DashboardLayout>
+    )
+};
+
+export default InterviewPrep;
